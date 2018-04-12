21c273226de80981a137a04dcb2f632063a48320
kabyr <eugenepro@icloud.com>
2018-04-12 23:04:38 +0300

    home

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,16 @@
 import sayHello from './lib/sayHello.js';
 
 sayHello();
+
+$('form input[type="file"]').change(function() {
+  var text = '';
+  var n = this.files.length;
+  if(n < 2) {
+    text = 'файл';
+  } else if(n >= 2 && n < 5) {
+    text = 'файла';
+  } else if(n >= 5 && n < 20) {
+    text = 'файлов';
+  }
+  $('form .file p').text(n + ' ' + text);
+});
