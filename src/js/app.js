879258f8f336e3f337844247901b3a1f6b20a995
kabyr <eugenepro@icloud.com>
2018-04-13 16:53:50 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,10 @@
 import './import/form.js';
 import './import/top.js';
 import jqueryModal from 'jquery-modal';
+
+$('a[rel]').click(function(event) {
+  $(this).modal({
+    fadeDuration: 250
+  });
+  return false;
+});
--- src/js/import/top.js
+++ src/js/import/top.js
@@ -3,3 +3,11 @@ $('.top').on('click', function() {
   $('html, body').stop().animate({scrollTop:0}, 1000, 'swing');
 
 });
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 450) {
+    $('.top').fadeIn();
+  } else{
+    $('.top').fadeOut();
+  }
+});
